5978368fbd4a44c84f5ec4a74fedaaf3ed35bc5d
sravan30190 <58553492+sravan30190@users.noreply.github.com>
2019-12-05 17:00:09 +0530

    Add files via upload
    command line arguments

---
+++ p3.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class P3
+{
+public static void main(String[] args)
+{
+System.out.println(args[0]+" "+args[1]);
+int q=Integer.parseInt(args[0]);
+int r=Integer.parseInt(args[1]);
+int s=q+r;
+System.out.println("the sum is"+s);
+}}
+
