69992e141c5ccf384b7f156cdf23fb36c59c0e51
sravan30190 <58553492+sravan30190@users.noreply.github.com>
2019-12-06 16:44:04 +0530

    encpusulation

--- main.java
+++ main.java
@@ -1,31 +1,84 @@
+
 import java.io.*;
-import java.lang.*;
 import java.util.*;
-class cal
+import java.lang.*;
+class author
 {
-static int intpower(int a, int b)
+private String name,email,gen;
+author(String name,String email,String gen)
 {
-int p=1;
-        while (b != 0)
-        {
-            p *= a;
-            b--;
-        }
-return p;
+name=this.name;
+email=this.email;
+gen=this.gen;
 }
-static double Dpower(double x, double y)
+String getname()
 {
-double q;
-q=Math.pow(x,y);
-return q;
+return name;
 }
-}
-class main
+String getemail()
 {
-public static void main(String[] ars)
+return auname;
+}
+String getgen()
 {
-cal pk=new cal();
-System.out.println("Integers power result:"+pk.intpower(4,5));
-System.out.println("Double power result:"+pk.Dpower(4.2,5.8));
+return gen;
+}
+
+}
+class book
+{
+private String bname,aname;
+private Double price;
+private int qunty;
+book(String bname,String aname)
+{
+this.bname=bname;
+this.aname=aname;
+}
+String getbname()
+{
+return bname;
+}
+String getaname()
+{
+return aname;
+}
+void setqty(int qunty)
+{
+this.qunty=qunty;
+}
+int getqty()
+{
+return qunty;
+}
+void setprice(Double price)
+{
+this.price=price;
+}
+Double getprice()
+{
+return price;
 }
 }
+class main{
+public static void main(String[] srvan)
+{
+author pk=new author("ravi","drbabu456@gmail.com","othres");
+book ps = new book("Sherlock Homles","Sir Arthur Conan Doyle");
+System.out.println("NAME OF THE BOOK:\n "+ps.getbname());
+System.out.println("NAME OF THE author:\n "+ps.getaname());
+ps.setprice(96.58);
+ps.setqty(10);
+System.out.println("PRICE:\n"+ps.getprice());
+System.out.println("QUNTITY:\n"+ps.getqty());
+System.out.println("NAME:\n"+pk.getname());
+System.out.println("GENDER:\n"+pk.getgen());
+System.out.println("EMAIL:\n"+pk.getemail());
+
+
+}
+}
+
+
+
+
